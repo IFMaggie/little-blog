200277b669d0392b534b8be658c3c29735684251
cuifang.hao <cuifang.hao@alo7.com>
2019-03-13 17:37:39 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="personal">
+      Test
+      
+    <!-- <router-view/> -->
   </div>
 </template>
-
 <script>
 export default {
-  name: "App"
+  name: "personal"
 };
 </script>
-
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.personal {
+    font-size: 24px;
+    color: brown;
 }
 </style>
